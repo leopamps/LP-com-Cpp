17b861ec3e4e028e1dda8fba5d7a97952181ee9d
leopamps <leo.pamplona96@hotmail.com>
2023-04-01 03:35:14 -0300

    completo

--- Lista1-mediaPares.cpp
+++ Lista1-mediaPares.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main() {
 
-    
+    float num1, num2;
+    for(int i = 1; i <= 10; i++){
+        cout << "Digite o par " << i << "/10" << endl;
+        cin >> num1;
+        cin >> num2;
+        cout << "media = " << (num1 + num2)/2 << endl << endl;
+    }
+
     return 0;
 }
